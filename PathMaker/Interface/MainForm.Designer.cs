ae5da1c5077769f859c8ca3d0d04bbb24f624b8d
Robi <alroberto@rambler.ru>
2017-03-31 00:21:01 +0300

    Доработана экранная форма: 1. Изъят функционал получения координат по топониму; 2. Изменены положения компонентов и их свойста. 3. Добавлены подсказки на текстовые поля.

--- PathMaker/Interface/MainForm.Designer.cs
+++ PathMaker/Interface/MainForm.Designer.cs
@@ -29,41 +29,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.streetTextBox = new System.Windows.Forms.TextBox();
-            this.requestButton = new System.Windows.Forms.Button();
             this.resultLabel = new System.Windows.Forms.Label();
             this.startStreet = new System.Windows.Forms.TextBox();
             this.endStreet = new System.Windows.Forms.TextBox();
-            this.requestButton2 = new System.Windows.Forms.Button();
-            this.GeocodingLabel = new System.Windows.Forms.Label();
+            this.directionRequestButton = new System.Windows.Forms.Button();
             this.DirecrionLabel = new System.Windows.Forms.Label();
             this.gMap = new GMap.NET.WindowsForms.GMapControl();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
-            // 
-            // streetTextBox
-            // 
-            this.streetTextBox.Location = new System.Drawing.Point(28, 51);
-            this.streetTextBox.Margin = new System.Windows.Forms.Padding(2);
-            this.streetTextBox.Name = "streetTextBox";
-            this.streetTextBox.Size = new System.Drawing.Size(100, 20);
-            this.streetTextBox.TabIndex = 0;
-            // 
-            // requestButton
-            // 
-            this.requestButton.Location = new System.Drawing.Point(28, 97);
-            this.requestButton.Margin = new System.Windows.Forms.Padding(2);
-            this.requestButton.Name = "requestButton";
-            this.requestButton.Size = new System.Drawing.Size(100, 19);
-            this.requestButton.TabIndex = 1;
-            this.requestButton.Text = "Найти";
-            this.requestButton.UseVisualStyleBackColor = true;
-            this.requestButton.Click += new System.EventHandler(this.requestButton_Click);
             // 
             // resultLabel
             // 
             this.resultLabel.AutoSize = true;
-            this.resultLabel.Location = new System.Drawing.Point(154, 51);
+            this.resultLabel.Location = new System.Drawing.Point(21, 47);
             this.resultLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.resultLabel.Name = "resultLabel";
             this.resultLabel.Size = new System.Drawing.Size(0, 13);
@@ -71,45 +52,41 @@
             // 
             // startStreet
             // 
-            this.startStreet.Location = new System.Drawing.Point(214, 51);
+            this.startStreet.Location = new System.Drawing.Point(98, 17);
             this.startStreet.Name = "startStreet";
             this.startStreet.Size = new System.Drawing.Size(100, 20);
             this.startStreet.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.startStreet, "Введите конечную улицу");
+            this.startStreet.MouseLeave += new System.EventHandler(this.startStreet_MouseLeave);
+            this.startStreet.MouseHover += new System.EventHandler(this.startStreet_MouseHover);
             // 
             // endStreet
             // 
-            this.endStreet.Location = new System.Drawing.Point(333, 51);
+            this.endStreet.Location = new System.Drawing.Point(224, 17);
             this.endStreet.Name = "endStreet";
             this.endStreet.Size = new System.Drawing.Size(100, 20);
             this.endStreet.TabIndex = 4;
+            this.endStreet.MouseLeave += new System.EventHandler(this.endStreet_MouseLeave);
+            this.endStreet.MouseHover += new System.EventHandler(this.endStreet_MouseHover);
             // 
-            // requestButton2
+            // directionRequestButton
             // 
-            this.requestButton2.Location = new System.Drawing.Point(273, 97);
-            this.requestButton2.Name = "requestButton2";
-            this.requestButton2.Size = new System.Drawing.Size(100, 23);
-            this.requestButton2.TabIndex = 5;
-            this.requestButton2.Text = "Найти";
-            this.requestButton2.UseVisualStyleBackColor = true;
-            this.requestButton2.Click += new System.EventHandler(this.requestButton2_Click);
-            // 
-            // GeocodingLabel
-            // 
-            this.GeocodingLabel.AutoSize = true;
-            this.GeocodingLabel.Location = new System.Drawing.Point(28, 33);
-            this.GeocodingLabel.Name = "GeocodingLabel";
-            this.GeocodingLabel.Size = new System.Drawing.Size(59, 13);
-            this.GeocodingLabel.TabIndex = 6;
-            this.GeocodingLabel.Text = "Geocoding";
+            this.directionRequestButton.Location = new System.Drawing.Point(364, 15);
+            this.directionRequestButton.Name = "directionRequestButton";
+            this.directionRequestButton.Size = new System.Drawing.Size(100, 23);
+            this.directionRequestButton.TabIndex = 5;
+            this.directionRequestButton.Text = "Найти";
+            this.directionRequestButton.UseVisualStyleBackColor = true;
+            this.directionRequestButton.Click += new System.EventHandler(this.directionRequestButton_Click);
             // 
             // DirecrionLabel
             // 
             this.DirecrionLabel.AutoSize = true;
-            this.DirecrionLabel.Location = new System.Drawing.Point(159, 51);
+            this.DirecrionLabel.Location = new System.Drawing.Point(21, 20);
             this.DirecrionLabel.Name = "DirecrionLabel";
-            this.DirecrionLabel.Size = new System.Drawing.Size(49, 13);
+            this.DirecrionLabel.Size = new System.Drawing.Size(64, 13);
             this.DirecrionLabel.TabIndex = 7;
-            this.DirecrionLabel.Text = "Direction";
+            this.DirecrionLabel.Text = "Поиск пути";
             // 
             // gMap
             // 
@@ -119,7 +96,7 @@
             this.gMap.GrayScaleMode = false;
             this.gMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMap.LevelsKeepInMemmory = 5;
-            this.gMap.Location = new System.Drawing.Point(12, 150);
+            this.gMap.Location = new System.Drawing.Point(12, 78);
             this.gMap.MarkersEnabled = true;
             this.gMap.MaxZoom = 20;
             this.gMap.MinZoom = 2;
@@ -132,9 +109,9 @@
             this.gMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMap.ShowTileGridLines = false;
-            this.gMap.Size = new System.Drawing.Size(533, 336);
+            this.gMap.Size = new System.Drawing.Size(533, 408);
             this.gMap.TabIndex = 8;
-            this.gMap.Zoom = 9D;
+            this.gMap.Zoom = 10D;
             this.gMap.Load += new System.EventHandler(this.gMap_Load);
             // 
             // MainForm
@@ -144,13 +121,10 @@
             this.ClientSize = new System.Drawing.Size(557, 513);
             this.Controls.Add(this.gMap);
             this.Controls.Add(this.DirecrionLabel);
-            this.Controls.Add(this.GeocodingLabel);
-            this.Controls.Add(this.requestButton2);
+            this.Controls.Add(this.directionRequestButton);
             this.Controls.Add(this.endStreet);
             this.Controls.Add(this.startStreet);
             this.Controls.Add(this.resultLabel);
-            this.Controls.Add(this.requestButton);
-            this.Controls.Add(this.streetTextBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
@@ -162,15 +136,14 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox streetTextBox;
-        private System.Windows.Forms.Button requestButton;
         private System.Windows.Forms.Label resultLabel;
         private System.Windows.Forms.TextBox startStreet;
         private System.Windows.Forms.TextBox endStreet;
-        private System.Windows.Forms.Button requestButton2;
-        private System.Windows.Forms.Label GeocodingLabel;
+        private System.Windows.Forms.Button directionRequestButton;
         private System.Windows.Forms.Label DirecrionLabel;
         private GMap.NET.WindowsForms.GMapControl gMap;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
     }
 }
 
--- PathMaker/Interface/MainForm.cs
+++ PathMaker/Interface/MainForm.cs
@@ -13,36 +13,19 @@ namespace StreetViewer.Interface
 {
     public partial class MainForm : Form
     {
-        private const string ERROR_MESSAGE = "Введены некорректные\r\n данные";
+        private const string ERROR_MESSAGE = "Введены некорректные данные";
+
         private Controller controller;
 
         public MainForm()
         {
             InitializeComponent();
             controller = new Controller();
+            setToolTipProperties(toolTip1);
+            setToolTipProperties(toolTip2);
         }
 
-        private void requestButton_Click(object sender, EventArgs e)
-        {
-            if (string.IsNullOrEmpty(streetTextBox.Text))
-            {
-                resultLabel.Text = ERROR_MESSAGE;
-            }
-            else
-            {
-                string result = controller.getGeocoding(streetTextBox.Text);
-                if (string.IsNullOrEmpty(result))
-                {
-                    resultLabel.Text = ERROR_MESSAGE;
-                }
-                else
-                {
-                    resultLabel.Text = result;
-                }
-            }
-        }
-
-        private void requestButton2_Click(object sender, EventArgs e)
+        private void directionRequestButton_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(startStreet.Text) || string.IsNullOrEmpty(endStreet.Text))
             {
@@ -68,6 +51,34 @@ namespace StreetViewer.Interface
             gMap.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
             gMap.SetPositionByKeywords("Moscow, Russia");
+        }
+
+        private void startStreet_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(startStreet, "Введите начальную улицу");
+        }
+
+        private void startStreet_MouseLeave(object sender, EventArgs e)
+        {
+            toolTip1.Hide(startStreet);
+        }
+
+        private void endStreet_MouseHover(object sender, EventArgs e)
+        {
+            toolTip2.SetToolTip(endStreet, "Введите конечную улицу");
+        }
+
+        private void endStreet_MouseLeave(object sender, EventArgs e)
+        {
+            toolTip1.Hide(endStreet);
+        }
+
+        private void setToolTipProperties(System.Windows.Forms.ToolTip toolTip)
+        {
+            toolTip.AutoPopDelay = 5000;
+            toolTip.InitialDelay = 1000;
+            toolTip.ReshowDelay = 500;
+            toolTip.ShowAlways = true;
         }
     }
 }
